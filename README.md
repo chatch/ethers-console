a179c2d422a20a873c107b0e0783e4f69927bc21
Chris Hatch <hatch@tuta.io>
2020-02-29 15:06:08 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# ether-console
+
+[![NPM Package](https://img.shields.io/npm/v/ethers-console.svg?style=flat-square)](https://www.npmjs.org/package/ethers-console)
+
+An Ethereum console using ethers.js.
+
+For now just sets up handles for each network using an Infura Provider for a quick start.
+
+## Usage
+
+```
+> ethers-console
+eth> mainnet.getTransactionReceipt('0x1234....')
+eth> kovan.getBlockNumber().then(num=>console.log(num))
+eth> local.getNetwork().then(num=>console.log(num))
+```
+
+Shortcuts:
+```
+eth> m    // mainnet
+eth> k    // kovan
+eth> r    // rinkeby
+eth> ro   // ropsten
+eth> l    // local   - localhost:8545 (JsonRpcProvider)
+eth> g    // ganache - localhost:7545 (JsonRpcProvider)
+```
+
+## Setup
+
+```
+> npm i -g ethers-console
+> cat > ~/.ethers-console.env
+INFURA_PROJECT_ID=<your infura.io project id>
+^D
+```
+
+## Todo
+
+* Signers - add private keys to ~/.ethers-console.env
---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+const ethers = require('ethers')
+const ospath = require('ospath')
+const path = require('path')
+const repl = require('repl')
+
+const configPath = path.join(ospath.home(), '.ethers-console.env')
+require('dotenv').config({ path: configPath })
+const infuraProjectId = process.env.INFURA_PROJECT_ID
+
+const { InfuraProvider, JsonRpcProvider } = ethers.providers
+
+const ethersMainnet = new InfuraProvider('mainnet', infuraProjectId)
+const ethersRinkeby = new InfuraProvider('rinkeby', infuraProjectId)
+const ethersKovan = new InfuraProvider('kovan', infuraProjectId)
+const ethersRopsten = new InfuraProvider('ropsten', infuraProjectId)
+
+const ethersLocal = new JsonRpcProvider(`http://localhost:8545`)
+const ethersGanache = new JsonRpcProvider(`http://localhost:7545`)
+
+const { context } = repl.start('eth> ')
+
+Object.assign(context, {
+  // mainnet
+  m: ethersMainnet,
+  main: ethersMainnet,
+  mainnet: ethersMainnet,
+
+  // testnets
+  k: ethersKovan,
+  kovan: ethersKovan,
+
+  r: ethersRinkeby,
+  rinkeby: ethersRinkeby,
+
+  ro: ethersRopsten,
+  ropsten: ethersRopsten,
+
+  // local
+  l: ethersLocal,
+  local: ethersLocal,
+
+  // development
+  g: ethersGanache,
+  ganache: ethersGanache,
+})
